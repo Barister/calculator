b6f5c43e5993c3e72833cd3db5f1a36b7ccf668c
Barister <59793749+Barister@users.noreply.github.com>
2023-04-16 21:02:57 +0300

    First sketches of JS without global idea

--- js/script.js
+++ js/script.js
@@ -1,6 +1,51 @@
-let number1;
-let number2;
+let currentNumber = 0;
+let previousNumber;
 let operator;
+
+const displayNumber = document.querySelector('.display__main');
+console.log('main__display:', displayNumber);
+
+const listenToButtons = function (event) {
+    if(event.target.closest('button')) {
+        let clickedButton = event.target.closest('button');
+
+        if (clickedButton.classList.contains('button__operate')) {
+            console.log('сработала кнопка operate!');
+        }
+        else if(clickedButton.classList.contains('button__int')) {
+            console.log('сработала кнопка int!');
+        }
+        else if(clickedButton.classList.contains('button__toggle')) {
+            console.log('сработала кнопка toggle!');
+            toggleButton();
+        }
+        else if(clickedButton.classList.contains('button__point')) {
+            console.log('сработала кнопка point!');
+        }
+    }
+}
+
+//buttons
+
+function toggleButton() {
+    if(currentNumber !== 0){
+        currentNumber = -currentNumber;
+        showDisplayNumber();
+    }
+    console.log('currentNumber', currentNumber);
+    showDisplayNumber();
+}
+
+const allButtons = document.querySelector('.calc__buttons');
+
+allButtons.addEventListener('click', event => listenToButtons(event));
+
+function showDisplayNumber () {
+    displayNumber.textContent = currentNumber;
+    console.log(displayNumber);
+}
+
+showDisplayNumber();
 
 function operate(number1, operator, number2) {
     
