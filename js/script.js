6f10bb52ce95c08d7038c2d27875a2c52319822b
Barister <59793749+Barister@users.noreply.github.com>
2023-04-28 20:16:31 +0300

    Finally fixed all possibilities I believe

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+// to determine variables
+
 let currentNumber = 0;
 let previousNumber = null;
 let operator = null;
 let lastOperator = null;
+let historyDisplay = '';
+let switcherOperate = false;
+let switcherEquals = false;
+let toShowNumber = null;
+let lastNumber = null;
 
 // console.log('type of null:', typeof null);
 // console.log('true of 0 == null:', 0 == null);
 
 
 const displayNumber = document.querySelector('.display__main');
+const displayHistory = document.querySelector('.display__history');
 const allButtons = document.querySelector('.calc__buttons');
 
 //console.log('main__display:', displayNumber);
@@ -19,109 +27,141 @@ const listenToButtons = function (event) {
         let clickedId = event.target.closest('button').id;
         //console.log('clickedButton.id:', clickedId);
 
-        if (clickedClass == 'button__operate') {
-            console.log('сработала кнопка operate!:', event.target.closest('button').innerText);
-            operateButton(event.target.closest('button').innerText);
+        switch (clickedClass) {
+            case 'button__operate':
+                // console.log('сработала кнопка operate!:', event.target.closest('button').innerText);
+                operateButton(event.target.closest('button').innerText);
+                break;
+            case 'button__clear':
+                // console.log('сработала кнопка clear!');
+                clearButton();
+                break;
+            case 'button__int':
+                // console.log('сработала кнопка int!');
+                numberButton(parseInt(clickedId));
+                break;
+            case 'button__toggle':
+                // console.log('сработала кнопка toggle!');
+                toggleButton();
+                break;
+            case 'button__point':
+                // console.log('сработала кнопка point!');
+                break;
+            case 'button__equals':
+                // console.log('сработала кнопка equals!');
+                equals();
+                break;
+            case 'button__backspace': 
+                // console.log('сработала кнопка backspace!');
+                backspaceButton();
+                break;
         }
-        else if(clickedClass == 'button__clear') {
-            console.log('сработала кнопка clear!');
-            clearButton();
-        }
-        else if(clickedClass == 'button__int') {
-            console.log('сработала кнопка int!');
-            numberButton(parseInt(clickedId));
-        }
-        else if(clickedClass == 'button__toggle') {
-            console.log('сработала кнопка toggle!');
-            toggleButton();
-        }
-        else if(clickedClass == 'button__point') {
-            console.log('сработала кнопка point!');
-        }
-        else if (clickedClass == 'button__equals') {
-            console.log('сработала кнопка equals!');
-            operate();
-        }
-        else if (clickedClass == 'button__backspace') {
-            console.log('сработала кнопка backspace!');
-            backspaceButton();
-        }
+        
     }
 }
 
-//buttons
+//if pressed numbers
 function numberButton(button) {
-    if (currentNumber === 0) {
+//  
+    if (switcherOperate === false && switcherEquals === true) {
+        console.log('бинго для кнопки цифры');
+        switcherEquals = false;
         currentNumber = button;
-        console.log('работает number1');
-    }
-    
-    else if (operator !== null && currentNumber === 0) {
-        previousNumber = currentNumber;
-        currentNumber = button;
-        operator = null;
-        console.log('работает number2');
-    }
-    
-    else if (currentNumber !== null && operator !== null && previousNumber !== null) {
-        if (currentNumber !== 0) {
-            currentNumber = parseInt(`${currentNumber}${button}`);
-            console.log('работает number3 с исключением');
-        }
-        else {
-        previousNumber = currentNumber;
-        currentNumber = button;
-        console.log('работает number3');
-        }
+        //operator = lastOperator;
+        historyDisplay = previousNumber+operator;
+        showHistory();
     }
 
-    else if (currentNumber !== null && operator == null && previousNumber !== null) {
-        previousNumber = currentNumber;
+    else if (switcherOperate === true) {
         currentNumber = button;
-        operator = lastOperator;
-        console.log('работает number4');
+        switcherOperate = false;
+        console.log("Сработало присвоение cN после switchOperate === true!");
     }
 
-    else if (currentNumber !== 0) {
+    else if (switcherEquals === true) {
+        currentNumber = button;
+        switcherEquals = false;
+        historyDisplay = '';
+        console.log("Сработало присвоение cN после switchEquals === true!");
+        showHistory();
+    }
+
+    else {
         currentNumber = parseInt(`${currentNumber}${button}`);
-        console.log('работает number5');
+        console.log("Сработала конкатенация числа!");
     }
 
-    
     showDisplayNumber();
+    
 }
 
+
+// нажатие кнопки с классом operate запускает функцию
 function operateButton(button) {
-    if (operator == null) {
+
+    //if operator == null , 
+    if (switcherEquals === true) {
+        console.log('switcherEquals === true');
+        operator = button;
+        currentNumber = previousNumber;
+        historyDisplay = previousNumber + operator;
+        showHistory();
+        switcherEquals = false;
+        switcherOperate = true;
+    }
+
+    else if (currentNumber && operator && previousNumber) {
+        //operator = button;
+        console.log('operator:', operator, typeof operator);
+        console.log('сработала условие: заполнены все значения', previousNumber, operator, currentNumber);
+        
+        equals();
+        operator = button;
+        //console.log(currentNumber); 
+    }
+
+    else if (switcherOperate === false) {
+        console.log('switcherOperate === false');
         operator = button;
         previousNumber = currentNumber;
         currentNumber = 0;
+        historyDisplay = previousNumber + operator;
+        switcherOperate = true;
+        console.log('historyDisplay:', historyDisplay, operator);
+        showHistory();
         console.log('operator присвоен:', operator)
     }
-    else if (currentNumber && operator && previousNumber) {
+
+    else {
+        console.log('Сработал последний вариант в operateButton');
         operator = button;
-        console.log('operator:', operator, typeof operator);
-        console.log('сработала условие: заполнены все значения', previousNumber, operator,currentNumber);
-        operate();
-        //console.log(currentNumber); 
+        historyDisplay = previousNumber + operator;
+        showHistory();
     }
-    //else if (){}
 }
 
 function clearButton(button) {
     operator = null;
     previousNumber = null;
     currentNumber = 0;
-    
+    lastNumber = null;
+    lastOperator = null;
+    historyDisplay = '';
+    switcherOperate = false;
+    switcherEquals = false;
+    showHistory();
     showDisplayNumber();
 }
 
 function toggleButton() {
     if(currentNumber !== 0){
-        currentNumber = -currentNumber;
-        showDisplayNumber();
+
+        currentNumber = -displayNumber.value;
+        historyDisplay = historyDisplay + currentNumber;
+        showHistory();
+        
     }
-    console.log('currentNumber', currentNumber);
+    console.log('сработала toggleButton, currentNumber =', currentNumber);
     showDisplayNumber();
 }
 
@@ -140,30 +180,42 @@ function backspaceButton() {
 allButtons.addEventListener('click', event => listenToButtons(event));
 
 function showDisplayNumber () {
-    displayNumber.textContent = currentNumber;
+    if (toShowNumber !== null) {
+        displayNumber.value = toShowNumber;
+        toShowNumber = null;
+        return;
+    }
+    else {
+        displayNumber.value = currentNumber;
+    }
+    
     //console.log(displayNumber);
 }
 
 showDisplayNumber();
 
-let lastNumber;
 
-function operate() {
+
+function equals() {
     console.log('запускаем уравнение');
     console.log('значения переменных:', 'pN', previousNumber, 'op', operator, 'cN',currentNumber);
     
-    if (!currentNumber && !previousNumber && !operator) {
+    if (currentNumber === 0 && previousNumber === null && operator === null) {
+        console.log('сработала равно return');
         return;
     }
     else if (!currentNumber) {
-        currentNumber = parseInt(displayNumber.innerText);
+        currentNumber = parseInt(displayNumber.value);
         console.log('присвоили currentNumner значение Экрана:', currentNumber);
     }
     else if(!operator) {
         operator = lastOperator;
         console.log('operator был пустой, взяли значение:', lastOperator);
     }
-    else if (!lastNumber) {lastNumber = currentNumber};
+    if (lastNumber === null) {
+        lastNumber = currentNumber
+        console.log('lastNumber:', lastNumber);
+    };
 
     switch (operator) {
         case '*': 
@@ -176,17 +228,35 @@ function operate() {
             break;
         case '+':
             console.log('sum');
-            currentNumber = previousNumber + currentNumber;
+            currentNumber += previousNumber;
             break;
         case '-':
             console.log('subtract');
             currentNumber = previousNumber - currentNumber;
             break;
     }
-    showDisplayNumber();
-    previousNumber = lastNumber;
+    toShowNumber = currentNumber;
+    historyDisplay = previousNumber + operator + lastNumber + '=';
+    console.log('historyDisplay:', historyDisplay);
+    previousNumber = currentNumber;
+    currentNumber = lastNumber;
+    switcherEquals = true;
+    lastNumber = null;
     lastOperator = operator;
+    console.log('lastOperator после equals():', lastOperator);
     operator = null;
-    console.log('после операции равно переменные:', 'pN', previousNumber, 'op', operator, 'cN',currentNumber);
+    console.log('после операции равно переменные:', 'pN:', previousNumber, 'op:', operator, 'cN:',currentNumber);
+    showDisplayNumber();
+    showHistory();
     
+    
+}
+
+
+
+//let's try to make historyDisplay
+
+function showHistory () {
+    console.log('сработала функция historyDisplay')
+    displayHistory.innerText = historyDisplay;
 }
